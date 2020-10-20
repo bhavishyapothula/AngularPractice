7e3215a377f1ea0f371402a201232d1e7fece146
bhavishyapothula <bhavishya.pothula@gmail.com>
2020-10-20 18:50:35 +0530

    Day7commit

--- Forms/Reactive-forms/src/app/app.component.ts
+++ Forms/Reactive-forms/src/app/app.component.ts
@@ -1,15 +1,102 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder,Validators,FormGroup,FormArray } from '@angular/forms';
+import { RegistrationService } from './registration.service';
+import { PasswordValidator } from './shared/password.validator';
+import { ForbiddenNameValidator } from './shared/user-name.validator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  registrationForm=new FormGroup({
-    userName:new FormControl('Bhavishya'),
-    password:new FormControl(''),
-    confirmPassword:new FormControl('')
-  })
+export class AppComponent implements OnInit {
+  
+  registrationForm: FormGroup;
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Bhavishya'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
+  constructor(private fb: FormBuilder,private _registrationService:RegistrationService) { }
+  ngOnInit()
+  {
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3),ForbiddenNameValidator(/admin/)]],
+      email:[''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      }),
+      alternateEmails:this.fb.array([])
+    },{validators:PasswordValidator});
+    this.registrationForm.get('subscribe').valueChanges
+    .subscribe(checkedValue=>{
+      const email=this.registrationForm.get('email');
+      if(checkedValue)
+      {
+        email.setValidators(Validators.required);
+      }
+      else{
+        email.clearValidators();
+      }
+      email.updateValueAndValidity();
+    });
+  }
+  
+
+ 
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
+  get alternateEmails() {
+    return this.registrationForm.get('alternateEmails')as FormArray;
+    }
+    addAlternateEmail()
+    {
+      this.alternateEmails.push(this.fb.control(''));
+    }
+
+ 
+  
+   loadAPIData() {
+    // this.registrationForm.setValue({
+    //   userName: 'Bruce',
+    //   password: 'test',
+    //   confirmPassword: 'test',
+    //   address: {
+    //     city: 'City',
+    //     state: 'State',
+    //     postalCode: '123456'
+    //   }
+    // });
+
+    this.registrationForm.patchValue({
+      userName: 'Bruce',
+      password: 'test',
+      confirmPassword: 'test'
+    });
+  }
+  onSubmit() {
+    console.log(this.registrationForm.value);
+    this._registrationService.register(this.registrationForm.value)
+      .subscribe(
+        response => console.log('Success!', response),
+        error => console.error('Error!', error)
+      );
+  }
+
 }
+
+
---
+++ Forms/Reactive-forms/src/app/shared/user-name.validator.ts
@@ -0,0 +1,15 @@
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+
+// export function ForbiddenNameValidator(control:AbstractControl): {[key:string]:any}|null {
+ 
+//     const forbidden = /admin/.test(control.value);
+//     return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+  
+// }
+
+export function ForbiddenNameValidator(forbiddenName: RegExp): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const forbidden = forbiddenName.test(control.value);
+    return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+  };
+}
